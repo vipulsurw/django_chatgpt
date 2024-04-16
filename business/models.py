dd61656572f25ce5b1b086379050623ee2573f55
vipulsurw <80000167+vipulsurw@users.noreply.github.com>
2024-04-16 22:23:31 +0100

    initial commit

--- business/models.py
+++ business/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Conversation(models.Model):
+    sender = models.CharField(max_length=15)
+    message = models.CharField(max_length=2000)
+    response = models.CharField(max_length=2000)
--- chatbot/settings.py
+++ chatbot/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'business',
 ]
 
 MIDDLEWARE = [
@@ -72,11 +73,16 @@ WSGI_APPLICATION = 'chatbot.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+from decouple import config
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'mydb',
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
