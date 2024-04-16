4fa747c3805f21a2662137b31bd793df4946c138
vipulsurw <80000167+vipulsurw@users.noreply.github.com>
2024-04-16 22:29:32 +0100

    db commit

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -77,7 +77,7 @@ from decouple import config
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mydb',
         'USER': config("DB_USER"),
         'PASSWORD': config("DB_PASSWORD"),
